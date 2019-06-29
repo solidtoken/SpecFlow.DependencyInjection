d5d5b55885a0c5d3245a8aa7acd8e868768d07d0
Mark Hoek <mark.hoek@solidtoken.nl>
2019-06-30 01:40:58 +0200

    Add initial implementation (#1)

---
+++ SpecFlow.DependencyInjection.Tests/DependencyInjectionPluginSteps.cs
@@ -0,0 +1,35 @@
+﻿using TechTalk.SpecFlow;
+using Xunit;
+
+namespace SolidToken.SpecFlow.DependencyInjection.Tests
+{
+	[Binding]
+	public class DependencyInjectionPluginSteps
+	{
+		private readonly ITestService testService;
+
+		public DependencyInjectionPluginSteps(ITestService testService)
+		{
+			this.testService = testService;
+		}
+
+		[Then(@"verify that TestService is correctly injected")]
+		public void ThenVerifyThatTestServiceIsCorrectlyInjected()
+		{
+			Assert.True(testService.Verify());
+		}
+	}
+
+	public interface ITestService
+	{
+		bool Verify();
+	}
+
+	public class TestService : ITestService
+	{
+		public bool Verify()
+		{
+			return true;
+		}
+	}
+}
