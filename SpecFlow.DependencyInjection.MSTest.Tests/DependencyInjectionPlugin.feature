26919aa255732b877e8a0ed9fe5538150f525e74
Mark Hoek <mark.hoek@solidtoken.nl>
2020-02-17 16:59:41 +0100

    Add initial tests based on MSTest testing framework (#24)
    Initial thought was that issue #24 was cause by using the MSTest testing framework. Although this was not the case, it does make sense to add tests based on MSTest testing framework.
    
    Still needs cleanup (rename the existing XUnit test project), support for both 2.1 and 3.1 SDK and investigate whether it is possible to code share between MSTest and XUnit test project.

---
+++ SpecFlow.DependencyInjection.MSTest.Tests/DependencyInjectionPlugin.feature
@@ -0,0 +1,8 @@
+﻿Feature: DependencyInjectionPlugin
+	As a developer I want to verify
+	that the plugin SolidToken.SpecFlow.DependencyInjection
+	allows me to use Microsoft.Extensions.DependencyInjection
+    inside the MSTest test framework
+
+Scenario: Test service injection
+	Then verify that TestService is correctly injected
---
+++ SpecFlow.DependencyInjection.MSTest.Tests/DependencyInjectionPluginSteps.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow.DependencyInjection.MSTest.Tests
+{
+    [Binding]
+    public class DependencyInjectionPluginSteps
+    {
+        private readonly ITestService testService;
+
+        public DependencyInjectionPluginSteps(ITestService testService)
+        {
+            this.testService = testService;
+        }
+
+        [Then(@"verify that TestService is correctly injected")]
+        public void ThenVerifyThatTestServiceIsCorrectlyInjected()
+        {
+            Assert.IsTrue(testService.Verify());
+        }
+    }
+
+    public interface ITestService
+    {
+        bool Verify();
+    }
+
+    public class TestService : ITestService
+    {
+        public bool Verify()
+        {
+            return true;
+        }
+    }
+}
