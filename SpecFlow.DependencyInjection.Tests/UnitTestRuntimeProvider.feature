cc8f75c660bb3716a80a12d6f3c432c5479e461d
Mark Hoek <mark.hoek@solidtoken.nl>
2022-05-07 15:51:22 +0200

    Fix missing IUnitTestRuntimeProvider (#85)
    Fixes: #73

---
+++ SpecFlow.DependencyInjection.Tests/UnitTestRuntimeProvider.feature
@@ -0,0 +1,6 @@
+﻿Feature: UnitTestRuntimeProvider
+    Issue #73: Unable to resolve IUnitTestRuntimeProvider
+    https://github.com/solidtoken/SpecFlow.DependencyInjection/issues/73
+
+Scenario: Assert IUnitTestRuntimeProvider is available
+	Then verify that IUnitTestRuntimeProvider is correctly injected
---
+++ SpecFlow.DependencyInjection.Tests/UnitTestRuntimeProviderSteps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.UnitTestProvider;
+using Xunit;
+
+namespace SolidToken.SpecFlow.DependencyInjection.Tests
+{
+    [Binding]
+    public class UnitTestRuntimeProviderSteps
+    {
+        private readonly IUnitTestRuntimeProvider _unitTestRuntimeProvider;
+
+        public UnitTestRuntimeProviderSteps(IUnitTestRuntimeProvider unitTestRuntimeProvider)
+        {
+            _unitTestRuntimeProvider = unitTestRuntimeProvider;
+        }
+
+        [Then(@"verify that IUnitTestRuntimeProvider is correctly injected")]
+        public void ThenVerifyThatIUnitTestRuntimeProviderIsCorrectlyInjected()
+        {
+            Assert.NotNull(_unitTestRuntimeProvider);
+        }
+    }
+}
