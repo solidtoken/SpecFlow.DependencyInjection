4d61d5520f5b2e05eab19d44af405838eb6e1b1e
Mark Hoek <mark.hoek@solidtoken.nl>
2022-05-22 12:37:20 +0200

    Use ISpecFlowOutputHelper instead of ITestOutputHelper
    Fixes: #75

---
+++ SpecFlow.DependencyInjection.Tests/ISpecFlowOutputHelperSteps.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SolidToken.SpecFlow.DependencyInjection.Tests
+{
+    [Binding]
+    public class ISpecFlowOutputHelperSteps
+    {
+        private readonly ISpecFlowOutputHelper _output;
+
+        public ISpecFlowOutputHelperSteps(ISpecFlowOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [When(@"a message is output using ISpecFlowOutputHelper")]
+        public void WhenAMessageIsOutputUsingISpecFlowOutputHelper()
+        {
+            _output.WriteLine("This is output from ISpecFlowOutputHelper");
+        }
+
+        [Then(@"verify that ISpecFlowOutputHelper is correctly injected")]
+        public void ThenVerifyThatISpecFlowOutputHelperIsCorrectlyInjected()
+        {
+            Assert.NotNull(_output);
+        }
+    }
+}
